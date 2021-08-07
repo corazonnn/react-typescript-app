eab08321ae42223d6792826a91c0e24b66483e3c
corazonnn <zero7364@icloud.com>
2021-08-07 12:05:35 +0900

    add: ログイン機能、ログイン画面

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,27 @@
-import { memo, VFC } from "react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+import { ChangeEvent, memo, useState, VFC } from "react";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { useAuth } from '../../hooks/useAuth';
 
-export const Login:VFC = memo(() => {
-return <p>ログインページ</p>
+export const Login: VFC = memo(() => {
+  const {login, loading} = useAuth();
+  const [userId, setUserId] = useState('');//型推論で勝手に推測してくれる
+  const onChangeUsrId =(e: ChangeEvent<HTMLInputElement>)=> setUserId(e.target.value);
+  
+  const onclickLogin = () => login(userId);
+
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザ管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="ユーザID" value={userId} onChange={onChangeUsrId} />
+          <PrimaryButton disabled={userId === ""} loading={loading} onClick={onclickLogin}>ログイン</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  )
 });
