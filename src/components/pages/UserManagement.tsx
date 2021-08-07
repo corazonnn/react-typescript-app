160ac7749e62179462eb24b6be027a32032db247
corazonnn <zero7364@icloud.com>
2021-08-07 13:56:01 +0900

    add: ユーザ一覧ページの実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,33 @@
-import { memo, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
+import { UserCard } from "../organisms/layout/user/UserCard";
+import { useAllUsers } from "../../hooks/userAllUsers"
 
-export const UserManagement:VFC = memo(() => {
-return <p>ユーザ管理画面</p>
+export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  //初回だけgetUsers()を実行する
+  useEffect(() => getUsers(), [])
+
+  return (
+    <>
+      {loading ?
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+        : (
+          <Wrap p={{ base: 4, md: 10 }} justify="center">
+            {users.map((user) => (
+              <WrapItem key={user.id} >
+                <UserCard
+                  imageUrl="https://source.unsplash.com/random"
+                  userName={user.username}
+                  fullName={user.name}
+                />
+              </WrapItem>
+            ))}
+          </Wrap>
+        )}
+    </>
+  )
 });
